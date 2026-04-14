c5b464d3f5c5088447a95d561b4974f972899c2b
Henrique Fagundes <riquefagundes11@gmail.com>
2026-04-14 19:48:04 -0300

    correção no erro da imagem e criação da classe de audio

---
+++ audio.py
@@ -0,0 +1,11 @@
+import winsound
+
+#classe para audio do game musica
+
+#classe audio_musica
+
+#classe audio_hit
+
+#classe audio_pulo
+
+#classe audio_gamerover
--- texture.py
+++ texture.py
@@ -1,10 +1,14 @@
 from OpenGL.GL import *
 from PIL import Image
+#import numpy as np
 
 def load_texture(path):
 
-    image = Image.open(path)
-    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    #image = Image.open(path)
+    #image = image.transpose(Image.FLIP_TOP_BOTTOM)
+
+    image = Image.open(path).transpose(Image.FLIP_TOP_BOTTOM).convert("RGBA")
+    #image_data = np.array(image).flatten()
 
     img_data = image.convert("RGBA").tobytes()
 
@@ -25,6 +29,8 @@ def load_texture(path):
         GL_UNSIGNED_BYTE,
         img_data
     )
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
