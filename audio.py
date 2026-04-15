7add4f8523890a20346f3b97850b09a61b3900bc
Henrique Fagundes <riquefagundes11@gmail.com>
2026-04-14 21:14:22 -0300

    correção no erro da imagem e criação da classe de audio

--- audio.py
+++ audio.py
@@ -1,12 +1,18 @@
 import winsound
+import os
 
 class AudioPulo:
     def __init__(self):
         self.caminho = "assets/audio_pulo.wav"
 
     def tocar(self):
-        winsound.PlaySound(self.caminho, winsound.SND_FILENAME | winsound.SND_ASYNC)
-
+        if os.path.exists(self.caminho):
+            winsound.PlaySound(
+                self.caminho,
+                winsound.SND_FILENAME | winsound.SND_ASYNC
+            )
+        else:
+            print("Arquivo de áudio não encontrado:", self.caminho)
 #classe para audio do game musica
 
 #classe audio_musica
