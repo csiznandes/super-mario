999a49573082566023e0f710a1845a54d165c42a
Henrique Fagundes <riquefagundes11@gmail.com>
2026-04-14 21:14:22 -0300

    correção no erro da imagem e criação da classe de audio

--- audio.py
+++ audio.py
@@ -1,17 +1,69 @@
 import winsound
 
+
+### Classes de Audio do game para eventos ###
+
+
 class AudioPulo:
+
+
     def __init__(self):
         self.caminho = "assets/audio_pulo.wav"
 
-    def tocar(self):
-        winsound.PlaySound(self.caminho, winsound.SND_FILENAME | winsound.SND_ASYNC)
 
+    def tocar(self):
+        winsound.PlaySound(
+        self.caminho,
+        winsound.SND_FILENAME | winsound.SND_ASYNC
+        )
+
+
+class AudioMusica:
+
+
+    def __init__(self):
+        self.caminho = "assets/audio_musica.wav"
+
+
+    def tocar(self):
+        winsound.PlaySound(
+        self.caminho,
+        winsound.SND_FILENAME | winsound.SND_ASYNC
+        )
+
+
+class AudioHit:
+
+
+    def __init__(self):
+        self.caminho = "assets/audio_hit.wav"
+
+
+    def tocar(self):
+        winsound.PlaySound(
+            self.caminho,
+            winsound.SND_FILENAME | winsound.SND_ASYNC
+        )
+
+
+
+class AudioGamerover:
+
+
+    def __init__(self):
+        self.caminho = "assets/audio_gamerover.wav"
+
+
+    def tocar(self):
+        winsound.PlaySound(
+        self.caminho,
+        winsound.SND_FILENAME | winsound.SND_ASYNC
+        )
+
+
+###################################
 #classe para audio do game musica
 
-#classe audio_musica
+#
 
-#classe audio_hit
-
-
-#classe audio_gamerover
+#
